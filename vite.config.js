1b8ee53b01ce0b8fc5443a2a093fbfb807246c89
kealeee <150263963+kealeee@users.noreply.github.com>
2025-06-10 15:07:55 +0800

    Updated vite.config.js (part 2)

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/leekeanhin-capstone/',
+  base: '/LeeKeanHin-Capstone/' // Match repo name exactly
 })
